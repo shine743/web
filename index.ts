79fb2b899642171d8fc0efdef665368803cdecbf
shine <chr70145@gmail.com>
2024-11-07 15:56:29 +0900

    ts 수정함

--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 // 기본 라우터 (정상 응답)
-app.get('/', (req, res) => {
-    res.send('Hello, world!');  // status(200)은 기본적으로 필요 없음
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Hello, world!');
 });
 
-// 404 에러 처리 라우터 (모든 라우터를 매칭 후, 404 처리)
-app.use((req, res) => {
+// 404 에러 처리 라우터
+app.use((req: Request, res: Response) => {
     res.status(404).send('Not Found');
 });
 
 // 서버 실행
-const PORT = process.env.PORT || 3000;  // 환경 변수로 포트 지정
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}...`);
+app.listen(80, () => {
+    console.log('Server is listening on port 80...');
 });
